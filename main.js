a825cc32e3ef5417f38e6484452a37d16a5641cd
axelnoche <105469738+axelnoche@users.noreply.github.com>
2023-02-07 20:03:57 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function setup() {
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    background("white");
+   // canvas.mouseReleased(classifyCanvas);
+    synth = window.speechsynthesis;
+}
+
+function preload() {
+
+
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
+function clearCanvas() {
+
+    background("white");
+}
